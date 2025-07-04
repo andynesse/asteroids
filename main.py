e36c269dc6f47a2a6772b0df7de2443b11f987c6
andynesse <andynesse04@gmail.com>
2025-07-04 17:35:08 +0200

    Add constants and setup gameloop

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import pygame
-
+from constants import *
 def main():
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("#000000")
+        
+        dt = clock.tick(60)/1000
+        pygame.display.flip()
     print("Starting Asteroids!")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+
 
 
 if __name__ == "__main__":
